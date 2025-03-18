dde5203f42624201b19d522e3d684a5614f3e807
Antonio Mauricio <antonioimportant@gmail.com>
2025-03-18 16:54:42 -0300

    feat: adds ResourceNotFound exception

---
+++ src/main/java/com/github/dedo_finger2/daynote/Service/Exception/ResourceNotFound.java
@@ -0,0 +1,7 @@
+package com.github.dedo_finger2.daynote.Service.Exception;
+
+public class ResourceNotFound extends RuntimeException {
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+}
