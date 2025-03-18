471b9fb97db58117872af9bf8bbcc8aab80c1346
Antonio Mauricio <antonioimportant@gmail.com>
2025-03-18 10:59:20 -0300

    chore: init

---
+++ src/main/java/com/github/dedo_finger2/daynote/DaynoteApplication.java
@@ -0,0 +1,13 @@
+package com.github.dedo_finger2.daynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaynoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaynoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dedo_finger2/daynote/DaynoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dedo_finger2.daynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaynoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
