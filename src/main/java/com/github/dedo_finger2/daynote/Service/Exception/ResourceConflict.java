a4f38bbe3c06713fd4e0341e5f12c1651a7055bf
Antonio Mauricio <antonioimportant@gmail.com>
2025-03-18 17:02:17 -0300

    feat: adds ResourceConflict exception

---
+++ src/main/java/com/github/dedo_finger2/daynote/Service/Exception/ResourceConflict.java
@@ -0,0 +1,7 @@
+package com.github.dedo_finger2.daynote.Service.Exception;
+
+public class ResourceConflict extends RuntimeException {
+    public ResourceConflict(String message) {
+        super(message);
+    }
+}
