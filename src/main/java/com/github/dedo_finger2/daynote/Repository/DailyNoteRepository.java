beb5b9dbd273e315d25087f6705e370a696a1b4d
Antonio Mauricio <antonioimportant@gmail.com>
2025-03-18 15:16:06 -0300

    feat: adds findByTitle into DailyNoteRepository interface

--- src/main/java/com/github/dedo_finger2/daynote/Repository/DailyNoteRepository.java
+++ src/main/java/com/github/dedo_finger2/daynote/Repository/DailyNoteRepository.java
@@ -3,5 +3,9 @@ package com.github.dedo_finger2.daynote.Repository;
 import com.github.dedo_finger2.daynote.Model.DailyNote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface DailyNoteRepository extends JpaRepository<DailyNote, Long> {
+    Optional<DailyNote> findByTitle(LocalDate title);
 }
